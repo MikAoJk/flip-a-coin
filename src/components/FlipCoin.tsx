af4bebc371a3afffc94e72249818746bfe62dbf6
Joakim Taule Kartveit <joakim.taule.kartveit@nav.no>
2024-09-12 09:21:09 +0200

    chore: added in black picutre on loading

--- src/components/FlipCoin.tsx
+++ src/components/FlipCoin.tsx
@@ -36,7 +36,7 @@ const FlipCoin = () => {
         <div className="flex flex-col items-center justify-center w-1/2 lg:w-1/3">
             <h1 className="mb-6 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
                 Flip a coin</h1>
-            {coinSideHead &&
+            {coinSideHead && !isEditing &&
                 <div>
                     <Image
                         src="heads.jpg"
@@ -47,7 +47,7 @@ const FlipCoin = () => {
                     <p className="mt-2 flex items-center justify-center">Heads</p>
                 </div>
             }
-            {!coinSideHead &&
+            {!coinSideHead && !isEditing &&
                 <div>
                     <Image
                         src="tails.jpg"
@@ -59,16 +59,25 @@ const FlipCoin = () => {
                 </div>
             }
             {isEditing &&
-                <button className="flex flex-row mt-6 bg-blue-700 text-white font-bold py-2 px-4 rounded" disabled>
-                    <svg className="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg"
-                         fill="none" viewBox="0 0 24 24">
-                        <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor"
-                                stroke-width="4"></circle>
-                        <path className="opacity-75" fill="currentColor"
-                              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-                    </svg>
-                    Fliping...
-                </button>
+                <div className="flex flex-col items-center justify-center">
+                    <Image
+                        src="black.jpg"
+                        alt="black bg"
+                        width={200}
+                        height={200}
+                    />
+                    <p className="mt-2 flex items-center justify-center">Heads or Tails</p>
+                    <button className="flex flex-row mt-6 bg-blue-700 text-white font-bold py-2 px-4 rounded" disabled>
+                        <svg className="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg"
+                             fill="none" viewBox="0 0 24 24">
+                            <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor"
+                                    stroke-width="4"></circle>
+                            <path className="opacity-75" fill="currentColor"
+                                  d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                        </svg>
+                        Fliping...
+                    </button>
+                </div>
             }
             {!isEditing &&
                 <button className="mt-6 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
