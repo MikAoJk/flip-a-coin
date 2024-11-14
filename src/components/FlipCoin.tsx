344925077deb823479a72a88613f9a74793d3551
Joakim Taule Kartveit <joakim.taule.kartveit@nav.no>
2024-11-14 12:35:13 +0100

    chore: added in a gif of the head and tails

--- src/components/FlipCoin.tsx
+++ src/components/FlipCoin.tsx
@@ -21,13 +21,13 @@ const FlipCoin = () => {
             setTimeout(() => {
                 setIsEditing(false)
                 setCoinSideHead(true)
-            }, 1000);
+            }, 1500);
         } else {
             setIsEditing(true)
             setTimeout(() => {
                 setIsEditing(false)
                 setCoinSideHead(false)
-            }, 1000);
+            }, 1500);
         }
 
     }
@@ -61,8 +61,8 @@ const FlipCoin = () => {
             {isEditing &&
                 <div className="flex flex-col items-center justify-center">
                     <Image
-                        src="black.jpg"
-                        alt="black bg"
+                        src="head-tails.gif"
+                        alt="head or tails image loop"
                         width={200}
                         height={200}
                     />
@@ -71,7 +71,7 @@ const FlipCoin = () => {
                         <svg className="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg"
                              fill="none" viewBox="0 0 24 24">
                             <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor"
-                                    stroke-width="4"></circle>
+                                    strokeWidth="4"></circle>
                             <path className="opacity-75" fill="currentColor"
                                   d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
                         </svg>
